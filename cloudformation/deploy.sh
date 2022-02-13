faceea2cf4a1e41d6d317075a2faa28270ce3a3f
Sheps <thesheps@gmail.com>
2022-02-13 15:46:57 +0000

    feat: cfn destroy script

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 aws cloudformation create-stack --template-body file://website.yaml --stack-name IacWorkshopCfn
-aws cloudformation update-stack --template-body file://website.yaml --stack-name IacWorkshopCfn
 
 BUCKET_DOMAIN_NAME=$(aws cloudformation describe-stacks \
     --stack-name IacWorkshopCfn \
